1a2aefd86d90ee0fb9ad4395bdd6c5b7a5627142
Jim Wright <jmwri93@gmail.com>
2017-10-11 11:37:18 +0100

    Use static type checking

--- runehistory/highscores.py
+++ runehistory/highscores.py
@@ -7,29 +7,40 @@ SKILLS_ORDER = ['overall', 'attack', 'defence', 'strength', 'hitpoints', 'ranged
               'herblore', 'agility', 'theiving', 'slayer', 'farming', 'hunter']
 
 
-def get(player):
-    """
-    Args:
-        player (str): Player name to lookup
+class Skill:
+    def __init__(self, name: str, rank: int, level: int, experience: int):
+        self.name = name
+        self.rank = rank
+        self.level = level
+        self.experience = experience
 
-    Returns:
-        HighScores
-    """
+
+class HighScores(dict):
+    def __init__(self, **kwargs: Skill):
+        for skill in kwargs.keys():
+            if skill not in SKILLS_ORDER:
+                raise AttributeError('{key} is not a valid skill'.format(key=skill))
+        super(HighScores, self).__init__(**kwargs)
+
+    def __setattr__(self, key: str, value):
+        if key not in SKILLS_ORDER:
+            raise AttributeError('{key} is not a valid skill'.format(key=key))
+        self[key] = value
+
+    def __getattr__(self, item: str):
+        if item not in SKILLS_ORDER:
+            raise AttributeError('{key} is not a valid skill'.format(key=item))
+        return self[item]
+
+
+def get(player: str) -> HighScores:
     response = requests.get('http://services.runescape.com/m=hiscore_oldschool/index_lite.ws', {
         'player': player
     })
     return parse_response(response.content.decode('utf-8'))
 
 
-def parse_response(data):
-    """
-    Parse the response from the API call into an object
-    Args:
-        data (str): Data from runescape highscores API
-
-    Returns:
-        HighScores
-    """
+def parse_response(data: str) -> HighScores:
     split_skills = data.split('\n')
     highscores = HighScores()
 
@@ -38,39 +49,3 @@ def parse_response(data):
         skill = Skill(skill_name, rank, level, experience)
         setattr(highscores, skill.name, skill)
     return highscores
-
-
-class Skill:
-    def __init__(self, name, rank, level, experience):
-        """
-        Args: Skill name
-            rank (int):
-            level (int):
-            experience (int):
-        """
-        self.name = name
-        self.rank = rank
-        self.level = level
-        self.experience = experience
-
-
-class HighScores(dict):
-    def __init__(self, **kwargs):
-        """
-        Args:
-            **kwargs (dict of str: Skill):
-        """
-        for skill in kwargs.keys():
-            if skill not in SKILLS_ORDER:
-                raise AttributeError('{key} is not a valid skill'.format(key=skill))
-        super(HighScores, self).__init__(**kwargs)
-
-    def __setattr__(self, key, value):
-        if key not in SKILLS_ORDER:
-            raise AttributeError('{key} is not a valid skill'.format(key=key))
-        self[key] = value
-
-    def __getattr__(self, item):
-        if item not in SKILLS_ORDER:
-            raise AttributeError('{key} is not a valid skill'.format(key=item))
-        return self[item]
